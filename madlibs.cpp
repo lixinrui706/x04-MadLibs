b898fa6f2cacc5d69d7cf6b6424b318c03c86a2a
lixinrui706 <xli21@mail.csuchico.edu>
2018-01-31 18:25:58 -0800

    the dog's story

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,30 @@
-//Authors: 
+//Authors:Xinrui Li 
 #include <iostream>
 #include <string>
 
 int main()
 {
+    std::string adjective;
+    double number;
+    std::string noun;
+    char character;
+
+    std::cout << "what is an adjective?";
+    std::cin >> adjective;
+
+    std::cout << "what is a number?";
+    std::cin >> number;
+
+    std::cout << "what is a noun?";
+    std::cin >> noun;
+
+    std::cout << "what is a character?";
+    std::cin >> character;
+
+    std::cout << "A "<< adjective << " dog ate a lot of bones," << std::endl;
+    std::cout << "the dog ate" << number << " bones."<< std::endl;
+    std::cout << "The dog's " << noun << " hit dog." << std::endl;
+    std::cout << "The dog's " << noun << " name is " << character << "." << std::endl;
 
   return 0;
 }
